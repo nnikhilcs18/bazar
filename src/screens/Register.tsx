ea9ecf751b3a2290e239d02ffdf34c45c8f8cd3f
NIKHIL N <nik@publicisgroupe.net>
2021-04-13 11:18:05 +0530

    .

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,59 +1,155 @@
-{/*import React from 'react';
-import {useState} from 'react';
+import React from 'react';
+import {useState, useEffect} from 'react';
 import {
   Image,
   View,
-  Text,
-  StyleSheet,
+  TouchableWithoutFeedback,
   KeyboardAvoidingView,
   Platform,
-  TouchableWithoutFeedback,
-  TextInput,
-  Keyboard,
   Alert,
+  Text,
+  StyleSheet,
 } from 'react-native';
 import {Input, Button} from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 // import colors from '../constants/colors';
-import styles from './styles/RegisterStyles'
+import styles from './styles/LoginStyles';
+import {useDispatch, useSelector, Provider} from 'react-redux';
+import {ApplicationState, onLogin} from '../redux';
+
 const Register = ({navigation}) => {
   const [hidePass, setHidePass] = useState(true);
+  const [data, setData] = React.useState({
+    isValidUser: true,
+    isValidPassword: true,
+    emailErrorMessage: '',
+    passwordErrorMessage: '',
+  });
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  {
+    /*const dispatch = useDispatch();
+
+  const {user, error} = useSelector(
+    (state: ApplicationState) => state.userReducer,
+  );
+
+  //fetch the token
+  const {token} = user;
+  //console.log(token);
+  const onTapLogin = () => {
+    dispatch(onLogin(email, password));
+  };
+*/
+  }
+
+  const handleValidEmail = (val: string) => {
+    if (val.length == 0) {
+      console.log('empty email');
+      setData({
+        ...data,
+        isValidUser: false,
+        emailErrorMessage: 'Email Cannot be Empty',
+      });
+    } else if (!val.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+      setData({
+        ...data,
+        isValidUser: false,
+        emailErrorMessage: 'Invalid Email Format',
+      });
+    } else {
+      setData({
+        ...data,
+        isValidUser: false,
+        emailErrorMessage: '',
+      });
+    }
+  };
+
+  //validating password front end
+  const handleValidPassword = (val: string) => {
+    if (val.length == 0) {
+      console.log('Password cannot be Empty');
+      setData({
+        ...data,
+        isValidPassword: false,
+        passwordErrorMessage: 'Password cannot be Empty',
+      });
+    } else if (
+      !val.match(
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+      )
+    ) {
+      setData({
+        ...data,
+        isValidPassword: false,
+        passwordErrorMessage:
+          'Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:',
+      });
+    } else {
+      setData({
+        ...data,
+        isValidPassword: true,
+        passwordErrorMessage: '',
+      });
+    }
+  };
 
   console.log('test');
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={styles.container}>
-      <Image
-        accessibilityLabel="SabkaBazar Logo"
-        style={styles.logo}
-        source={require('../images/logo.png')}
-      />
-
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.inner}>
-           <Text style={styles.header}>SignUp</Text> 
-          <TextInput
+      <Image style={styles.logo} source={require('../images/logo.png')} />
+      {/* <TouchableWithoutFeedback onPress={Keyboard.dismiss}> */}
+      <View style={styles.inner}>
+        <View>
+        <Input
             accessibilityLabel="Please Enter your First Name"
-            placeholder="First Name"
-            style={styles.textInput}
+            placeholder="Enter Your First Name"
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType={'email-address'}
+            onChangeText={setEmail}
+            onEndEditing={e => handleValidEmail(e.nativeEvent.text)}
+            onFocus={() => {
+              data.isValidUser = true;
+            }}
           />
-          <TextInput
-            accessibilityLabel="Please Enter your Last Name"
-            placeholder="Last Name"
-            style={styles.textInput}
-          />
-          <TextInput
-            accessibilityLabel="Please Enter your Email"
-            placeholder="Email"
-            style={styles.textInput}
-          />
-
           <Input
-            accessibilityLabel="Please Enter your Password"
+            accessibilityLabel="Please Enter your last name"
+            placeholder="Enter Your Last Name"
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType={'email-address'}
+            onChangeText={setEmail}
+            onEndEditing={e => handleValidEmail(e.nativeEvent.text)}
+            onFocus={() => {
+              data.isValidUser = true;
+            }}
+          />
+          <Input
+            accessibilityLabel="Please Enter your Email"
+            placeholder="Enter Your Email"
+            leftIcon={<Icon name="mail" size={24} color="#bf2957" />}
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType={'email-address'}
+            onChangeText={setEmail}
+            onEndEditing={e => handleValidEmail(e.nativeEvent.text)}
+            onFocus={() => {
+              data.isValidUser = true;
+            }}
+          />
+          {data.isValidUser ? null : (
+            <Text style={styles.errorMsg}>{data.emailErrorMessage}</Text>
+          )}
+          <Input
+            accessibilityLabel="Please Enter your password"
             placeholder="Password"
-            //style={styles.textInput}
             secureTextEntry={hidePass ? true : false}
+            leftIcon={<Icon name="lock" size={24} color="#bf2957" />}
             rightIcon={
               <Icon
                 name={hidePass ? 'visibility-off' : 'visibility'}
@@ -62,38 +158,72 @@ const Register = ({navigation}) => {
                 onPress={() => setHidePass(!hidePass)}
               />
             }
+            autoCapitalize="none"
+            autoCorrect={false}
+            onChangeText={setPassword}
+            onEndEditing={e => handleValidPassword(e.nativeEvent.text)}
+            onFocus={() => {
+              data.isValidPassword = true;
+            }}
           />
-          <TextInput
-            accessibilityLabel="Please re-enter your password for confirmation"
-            placeholder="Confirm Password"
-            //inputStyle={styles.textInput}
-            secureTextEntry={true}
+          {data.isValidPassword ? null : (
+            <Text style={styles.errorMsg}>{data.passwordErrorMessage} </Text>
+          )}
+          <Input
+            accessibilityLabel="Please Re Enter your password"
+            placeholder="COnfirm Password"
+            secureTextEntry={hidePass ? true : false}
+            leftIcon={<Icon name="lock" size={24} color="#bf2957" />}
+            rightIcon={
+              <Icon
+                name={hidePass ? 'visibility-off' : 'visibility'}
+                size={25}
+                color="#bf2957"
+                onPress={() => setHidePass(!hidePass)}
+              />
+            }
+            autoCapitalize="none"
+            autoCorrect={false}
+            onChangeText={setPassword}
+            onEndEditing={e => handleValidPassword(e.nativeEvent.text)}
+            onFocus={() => {
+              data.isValidPassword = true;
+            }}
           />
+          {data.isValidPassword ? null : (
+            <Text style={styles.errorMsg}>{data.passwordErrorMessage} </Text>
+          )}
+
           <Button
-            accessibilityLabel="Click to Register"
-            buttonStyle={styles.registerBtn}
-            title="Create New Account"
-            onPress={()=>navigation.navigate('Login')}
+            buttonStyle={styles.register}
+            title="Login"
+            onPress={() => navigation.navigate('Login')}
           />
-          <Text>
-            Already a Member?
-            <TextInput
-              style={styles.signInText}
-              onPressIn={()=>navigation.navigate('Categories')}>
-              SignIn
-            </TextInput>
-          </Text>
         </View>
-      </TouchableWithoutFeedback>
+
+        <Button
+          buttonStyle={styles.register}
+          title="Create New Account"
+          onPress={() => navigation.navigate('Login')}
+        />
+      </View>
+
+      {/* </TouchableWithoutFeedback>*/}
     </KeyboardAvoidingView>
   );
 };
 
-
 export default Register;
 
-*/ }
-import React from 'react';
+
+
+
+
+
+
+
+{/*
+  import React from 'react';
 import {useState} from 'react';
 import {
   Image,
@@ -111,7 +241,7 @@ import {Input, Button} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import colors from '../constants/colors';
 import styles from './styles/RegisterStyles'
-const Register = (navigation) => {
+const Register = ({navigation}) => {
   const [hidePass, setHidePass] = useState(true);
 
   console.log('test');
@@ -127,7 +257,6 @@ const Register = (navigation) => {
 
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.inner}>
-          {/* <Text style={styles.header}>SignUp</Text> */}
           <TextInput
             accessibilityLabel="Please Enter your First Name"
             placeholder="First Name"
@@ -183,6 +312,8 @@ const Register = (navigation) => {
     </KeyboardAvoidingView>
   );
 };
+
+*/}
 
 
 export default Register;
