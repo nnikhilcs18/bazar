a90ea92b0151d4c48695db892da026750f0c7389
Samyaka Pandey <sampande@publicisgroupe.net>
2021-04-22 11:50:22 +0530

    code cleanup

--- src/redux/reducer/product.ts
+++ src/redux/reducer/product.ts
@@ -1,36 +1,26 @@
+import {ADD_PRODUCT, INCREMENT, DECREMENT} from '../actions/actionTypes';
 
-import {ADD_PRODUCT,INCREMENT,DECREMENT} from '../actions/actionTypes';
-
-
-const initialState = 
-  {
-    
-    cartItems:[],
-    Counter: 0,
-    billAmount: 0,
-  }
+const initialState = {
+  cartItems: [],
+  Counter: 0,
+  billAmount: 0,
+};
 
 function productReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_PRODUCT:
-     
       let updatedCartItems = state.cartItems;
-   
-     
+
       let existingItemIndex = updatedCartItems.findIndex(
         element => element.ID == action.payload.ID,
       );
-      
-      if (existingItemIndex>=0) {
-        
-        
+
+      if (existingItemIndex >= 0) {
         action.payload.productQuantity =
           updatedCartItems[existingItemIndex].productQuantity + 1;
         action.payload.totalPrice =
-          action.payload.price +
-          updatedCartItems[existingItemIndex].totalPrice;
+          action.payload.price + updatedCartItems[existingItemIndex].totalPrice;
         updatedCartItems[existingItemIndex] = action.payload;
-        
       } else {
         action.payload.productQuantity = 1;
         action.payload.totalPrice = action.payload.price;
@@ -43,52 +33,45 @@ function productReducer(state = initialState, action) {
         billAmount: state.billAmount + action.payload.price,
       };
 
-      
-      case INCREMENT:
-    let dummyCartItems = state.cartItems;
-    console.log(dummyCartItems);
-      dummyCartItems.map((item) => {
+    case INCREMENT:
+      let dummyCartItems = state.cartItems;
+      console.log(dummyCartItems);
+      dummyCartItems.map(item => {
         if (action.payload.ID === item.ID) {
           item.productQuantity++;
         }
       });
-      return  {
-          ...state,
+      return {
+        ...state,
         cartItems: [...dummyCartItems],
-        billAmount:state.billAmount+action.payload.price,
+        billAmount: state.billAmount + action.payload.price,
         Counter: state.Counter + 1,
-       
       };
- 
- case DECREMENT:
-    dummyCartItems = state.cartItems;
-    if (action.payload.productQuantity === 1) {
+
+    case DECREMENT:
+      dummyCartItems = state.cartItems;
+      if (action.payload.productQuantity === 1) {
         dummyCartItems = state.cartItems.filter(
-          (item) => item.ID !== action.payload.ID,
+          item => item.ID !== action.payload.ID,
         );
       } else {
-        dummyCartItems.map((item) => {
+        dummyCartItems.map(item => {
           if (action.payload.ID === item.ID) {
             item.productQuantity--;
-            
-            
           }
         });
       }
       return {
-          ...state,
+        ...state,
         cartItems: [...dummyCartItems],
-       
-        billAmount:state.billAmount-action.payload.price,
+
+        billAmount: state.billAmount - action.payload.price,
         Counter: state.Counter - 1,
       };
-      
-    
-    default:
-      return state
-  }
 
-  
+    default:
+      return state;
+  }
 }
 
-export default productReducer
+export default productReducer;
--- src/screens/cart.tsx
+++ src/screens/cart.tsx
@@ -1,105 +1,116 @@
-
 import React, {useState, useEffect} from 'react';
-import {useSelector,useDispatch} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux';
 import {
- ItemsSubscript,
- ImageCart,
- ContainerCart,
- TopBar,
- Main,
- TextView,
- Increments,
- ItemsBar,
- ItemHeading,
- CheckOutButton,
- Url,
- OutputText,
- OutputView,
- EmptyCart,
- EmptyCartText,
- EmptyCartTextSub,
- CheckOutView,
- CheckOutText,
- InnerText,
- Text1,
- ButtonCart,
-} from '../constants/constantFile'
+  ItemsSubscript,
+  ImageCart,
+  ContainerCart,
+  TopBar,
+  Main,
+  TextView,
+  Increments,
+  ItemsBar,
+  ItemHeading,
+  CheckOutButton,
+  Url,
+  OutputText,
+  OutputView,
+  EmptyCart,
+  EmptyCartText,
+  EmptyCartTextSub,
+  CheckOutView,
+  CheckOutText,
+  InnerText,
+  Text1,
+  ButtonCart,
+} from '../constants/constantFile';
 import {Button, Text, View, FlatList, Image} from 'react-native';
-import { decrement, increment } from '../redux/actions/actions';
- 
-const Cart = () => {
- const cartItems = useSelector((state) => state);
- const billedAmount = useSelector((state)=> state);
- const dispatch = useDispatch()
- if(cartItems.productReducer.cartItems.length)
- {
-  return (
-    <Main accessible={true}>
-    <TextView accessible={true}>
-    <TopBar>
-    My Cart  <ItemsSubscript>({cartItems.productReducer.cartItems.length} items)</ItemsSubscript> 
-    </TopBar>
-    </TextView>
-    <FlatList
-    horizontal={false}
-    keyExtractor={data => data.ID}
-    data={cartItems.productReducer.cartItems}
-    renderItem={({item}) => {
-    return (
-    <ItemsBar accessible={true}>
-    <ImageCart
-    source={{
-    uri: item.image
-    }}
-    />
-    <ContainerCart accessible={true}>
-    <ItemHeading numberOfLines={3}>{item.productName}</ItemHeading>
-    <Increments accessible={true}>
-     <View style={{width:'15%'}}>
-    <ButtonCart  accessible={true} accessibilityLabel="Press to decrement!" color="#bf2957" onPress={() => dispatch(decrement(item))} title="-" />
-    </View>
-   
-    <OutputView accessible={true}>
-     <OutputText>{item.productQuantity}</OutputText>
-    </OutputView>
-    
-    <View style={{width:'15%'}}>
-    <ButtonCart   accessible={true} accessibilityLabel="Press to increment" color="#bf2957" onPress={() => dispatch(increment(item))} title="+" />
-    </View>
-    <OutputText> X Rs.{item.price}</OutputText>
-    </Increments>
-    
-    </ContainerCart>
-    </ItemsBar>
-    );
-    }}
-    />
-    <CheckOutView>
-    <CheckOutText>Promo code can be applied on payment page</CheckOutText>
-    <CheckOutButton accessible={true}
-     accessibilityLabel="Tap me! to check out" >
-       <InnerText style={{justifyContent:"space-around"}}>
-         <Text1>Proceed to checkout</Text1> 
-      <Text1>
-      {billedAmount.productReducer.billAmount}.Rs 
-      </Text1>
-      </InnerText>
-      </CheckOutButton>
-     </CheckOutView>
-    </Main>
-    );
+import {decrement, increment} from '../redux/actions/actions';
 
- }
-else{
-  return(
-  <EmptyCart accessible={true}>
-    <EmptyCartText>No items in your cart</EmptyCartText>
-    <EmptyCartTextSub>Your favourite items are just a click away</EmptyCartTextSub>
-  </EmptyCart>
-  );
-}
- 
- 
+const Cart = () => {
+  const cartItems = useSelector(state => state);
+  const billedAmount = useSelector(state => state);
+  const dispatch = useDispatch();
+  if (cartItems.productReducer.cartItems.length) {
+    return (
+      <Main accessible={true}>
+        <TextView accessible={true}>
+          <TopBar>
+            My Cart{' '}
+            <ItemsSubscript>
+              ({cartItems.productReducer.cartItems.length} items)
+            </ItemsSubscript>
+          </TopBar>
+        </TextView>
+        <FlatList
+          horizontal={false}
+          keyExtractor={data => data.ID}
+          data={cartItems.productReducer.cartItems}
+          renderItem={({item}) => {
+            return (
+              <ItemsBar accessible={true}>
+                <ImageCart
+                  source={{
+                    uri: item.image,
+                  }}
+                />
+                <ContainerCart accessible={true}>
+                  <ItemHeading numberOfLines={3}>
+                    {item.productName}
+                  </ItemHeading>
+                  <Increments accessible={true}>
+                    <View style={{width: '15%'}}>
+                      <ButtonCart
+                        accessible={true}
+                        accessibilityLabel="Press to decrement!"
+                        color="#bf2957"
+                        onPress={() => dispatch(decrement(item))}
+                        title="-"
+                      />
+                    </View>
+
+                    <OutputView accessible={true}>
+                      <OutputText>{item.productQuantity}</OutputText>
+                    </OutputView>
+
+                    <View style={{width: '15%'}}>
+                      <ButtonCart
+                        accessible={true}
+                        accessibilityLabel="Press to increment"
+                        color="#bf2957"
+                        onPress={() => dispatch(increment(item))}
+                        title="+"
+                      />
+                    </View>
+                    <OutputText> X Rs.{item.price}</OutputText>
+                  </Increments>
+                </ContainerCart>
+              </ItemsBar>
+            );
+          }}
+        />
+        <CheckOutView>
+          <CheckOutText>Promo code can be applied on payment page</CheckOutText>
+          <CheckOutButton
+            accessible={true}
+            accessibilityLabel="Tap me! to check out">
+            <InnerText style={{justifyContent: 'space-around'}}>
+              <Text1>Proceed to checkout</Text1>
+              <Text1>{billedAmount.productReducer.billAmount}.Rs</Text1>
+            </InnerText>
+          </CheckOutButton>
+        </CheckOutView>
+      </Main>
+    );
+  } else {
+    return (
+      <EmptyCart accessible={true}>
+        <EmptyCartText>No items in your cart</EmptyCartText>
+        <EmptyCartTextSub>
+          Your favourite items are just a click away
+        </EmptyCartTextSub>
+      </EmptyCart>
+    );
+  }
 };
- 
+
 export default Cart;
